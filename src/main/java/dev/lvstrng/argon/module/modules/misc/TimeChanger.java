ee63607e2bc613ecb6c530b7e9bfe4fad97208b4
Exo <ayhan17can@gmail.com>
2025-02-10 00:22:31 +0300

    time changer

---
+++ src/main/java/dev/lvstrng/argon/module/modules/misc/TimeChanger.java
@@ -0,0 +1,29 @@
+package dev.lvstrng.argon.module.modules.misc;
+
+import dev.lvstrng.argon.event.events.TickListener;
+import dev.lvstrng.argon.module.Category;
+import dev.lvstrng.argon.module.Module;
+import dev.lvstrng.argon.utils.EncryptedString;
+
+public final class TimeChanger extends Module implements TickListener {
+    public TimeChanger() {
+        super(EncryptedString.of("TimeChanger"), EncryptedString.of("Changes time of the world"), -1, Category.MISC);
+    }
+
+    @Override
+    public void onEnable() {
+        eventManager.add(TickListener.class, this);
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        eventManager.remove(TickListener.class, this);
+        super.onDisable();
+    }
+
+    @Override
+    public void onTick() {
+        mc.player.setSprinting(mc.player.input.pressingForward);
+    }
+}
